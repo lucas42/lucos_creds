24718d7b86ed5c86e84f6a09c1a5bb0e260db552
Luke Blaney <git@lukeblaney.co.uk>
2024-01-18 01:18:02 +0000

    Improve logging Switch to using slog for all logs. Replace panic calls with log and exit. Enable debug logs for now.

--- src/main.go
+++ src/main.go
@@ -4,8 +4,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
-	"log"
+	"log/slog"
 	"net"
 	"os"
 	"golang.org/x/crypto/ssh"
@@ -16,18 +15,19 @@ import (
  * If that fails, a new public/private key pair is generated and saved to the volume
  *
  * @returns ssh.Singer
- * (No errors are return, the function just panics.  Nothing else in the programme is gonna work without a private key)
+ * (No errors are returned - instead any failures log and then exit.  Nothing else in the programme is gonna work without a private key)
  */
 func getCreatePrivateKey() (ssh.Signer) {
 	privateKeyPath := "/var/lib/creds_store/id_rsa"
 	publicKeyPath := "/var/lib/creds_store/id_rsa.pub"
 	privateKeyBytes, err := os.ReadFile(privateKeyPath)
 	if err != nil {
-		log.Println("Failed to load private key, generating a new one")
+		slog.Warn("Failed to load private key, generating a new one")
 		bitSize := 4096
 		key, err := rsa.GenerateKey(rand.Reader, bitSize)
 		if err != nil {
-			panic(err)
+			slog.Error("Failed to generate RSA keypair", slog.Any("error", err))
+			os.Exit(4)
 		}
 
 		// Encode private key
@@ -41,7 +41,8 @@ func getCreatePrivateKey() (ssh.Signer) {
 		// Write private key to file
 		err = os.WriteFile(privateKeyPath, privateKeyBytes, 0700)
 		if err != nil {
-			panic(err)
+			slog.Error("Failed to write private key to filesystem", slog.Any("error", err))
+			os.Exit(4)
 		}
 
 		// Encode public key
@@ -55,43 +56,51 @@ func getCreatePrivateKey() (ssh.Signer) {
 		// Write public key to file.
 		err = os.WriteFile(publicKeyPath, publicKeyBytes, 0755)
 		if err != nil {
-			panic(err)
+			slog.Error("Failed to write public key to filesystem", slog.Any("error", err))
+			os.Exit(4)
 		}
 	}
 
 	privateKey, err := ssh.ParsePrivateKey(privateKeyBytes)
 	if err != nil {
-		panic(err)
+		slog.Error("Failed to parse private key", slog.Any("error", err))
+		os.Exit(5)
 	}
 
 	return privateKey
 }
 
-func acceptSshConnection(socket net.Listener, config *ssh.ServerConfig, logger *log.Logger, warn *log.Logger) {
-		connection, err := socket.Accept()
-		if err != nil {
-			warn.Println(err)
-			return
-		}
+func acceptSshConnection(socket net.Listener, config *ssh.ServerConfig) {
+	slog.Debug("Accepting new connection from socket")
+	connection, err := socket.Accept()
+	if err != nil {
+		slog.Warn("Failed to accept connection from socket", slog.Any("error", err))
+		return
+	}
 
-		sshConnection, channels, requests, err := ssh.NewServerConn(connection, config)
-		if err != nil {
-			warn.Println(err)
-			return
-		}
-		logger.Println("Login from user: ", sshConnection.User())
+	sshConnection, channels, requests, err := ssh.NewServerConn(connection, config)
+	if err != nil {
+		slog.Warn("Failed to create a new server connection", slog.Any("error", err))
+		return
+	}
+	slog.Debug("Login", "user", sshConnection.User())
 
-		go ssh.DiscardRequests(requests)
+	go ssh.DiscardRequests(requests)
 
-		for newChannel := range channels {
-			logger.Println("Incoming channel: ", newChannel.ChannelType())
-			// TODO: actually do some stuff here
-		}
+	for newChannel := range channels {
+		slog.Debug("Incoming channel", "channelType", newChannel.ChannelType())
+		// TODO: actually do some stuff here
+	}
 }
 
 func main() {
-	logger := log.New(os.Stdout, "LOG:", log.Ldate|log.Ltime|log.Lshortfile)
-	warn := log.New(os.Stderr, "WARN:", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Check for DEBUG environment variable to drop the log level to Debug
+	if os.Getenv("DEBUG") != "" {
+		// Can be replaced with `slog.SetLogLoggerLevel(slog.LevelDebug)` in golang 1.22
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	}
+
 	config := &ssh.ServerConfig{
 		NoClientAuth: true,  // TODO: Implement some client authentication
 	}
@@ -100,17 +109,19 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		warn.Fatal("Environment variable `PORT` not set")
+		slog.Error("Environment variable `PORT` not set")
+		os.Exit(2)
 	}
 
 	socket, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		warn.Fatal("Failed to listen for connection", err)
+		slog.Error("Failed to listen for connection", slog.Any("error", err))
+		os.Exit(3)
 	}
-	fmt.Printf("Listening on %v\n", socket.Addr())
+	slog.Info("Listening for connections", "address", socket.Addr())
 
 	for {
 		// Handle each incoming connection in its own goroutine
-		go acceptSshConnection(socket, config, logger, warn)
+		go acceptSshConnection(socket, config)
 	}
 }
