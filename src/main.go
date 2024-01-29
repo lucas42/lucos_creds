d2d1c7162f16fead123a4d2d4d25c5c11437254d
Luke Blaney <git@lukeblaney.co.uk>
2024-01-29 19:45:04 +0000

    add new main.go file which calls the logic in server.go

---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"log/slog"
+	"os"
+)
+
+
+func main() {
+
+	// Check for DEBUG environment variable to drop the log level to Debug
+	if os.Getenv("DEBUG") != "" {
+		// Can be replaced with `slog.SetLogLoggerLevel(slog.LevelDebug)` in golang 1.22
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		slog.Error("Environment variable `PORT` not set")
+		os.Exit(2)
+	}
+	startSftpServer(port, getCreatePrivateKey())
+
+}
