496241531495e0ce39f028304cbd567a50e3dc5e
Luke Blaney <git@lukeblaney.co.uk>
2024-01-29 21:45:07 +0000

    Add basic test of downloading a file using scp NB: not sure if it'll work in other environments (eg circleCI) as only OpenSSH 9.0 and above supports SFTP protocol as default for scp

--- src/main_test.go
+++
@@ -1,7 +0,0 @@
-package main
-import (
-	"testing"
-)
-
-func TestNothingYet(test *testing.T) {
-}
---
+++ src/server_test.go
@@ -0,0 +1,69 @@
+package main
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"os/exec"
+	"runtime/debug"
+	"testing"
+	"golang.org/x/crypto/ssh"
+)
+
+func assertEqual(test *testing.T, message string, expected interface{}, actual interface{}) {
+	if expected != actual {
+		test.Errorf("%s. Expected: %s, Actual: %s", message, expected, actual)
+	}
+}
+func assertNoError(test *testing.T, err error) {
+	if err != nil {
+		test.Errorf("Error returned: %s", err)
+		debug.PrintStack()
+	}
+}
+
+/**
+ * Creates a private key for use in the test only
+ * (doesn't persist to disk)
+ */
+func getPrivateKeyForTest(test *testing.T) (ssh.Signer) {
+	bitSize := 4096
+	key, err := rsa.GenerateKey(rand.Reader, bitSize)
+	assertNoError(test, err)
+	privateKeyBytes := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	)
+	privateKey, err := ssh.ParsePrivateKey(privateKeyBytes)
+	assertNoError(test, err)
+	return privateKey
+}
+
+func TestReadEnvFile(test *testing.T) {
+	port := "2222"
+	go startSftpServer(port, getPrivateKeyForTest(test))
+	user := "bob"
+	testFileName := "test.env"
+	cmd := exec.Command(
+		"/usr/bin/scp", 
+		"-o BatchMode=yes", 
+		"-o StrictHostKeyChecking=no", 
+		"-o UserKnownHostsFile=/dev/null", 
+		"-P "+port, 
+		user+"@localhost:.env", 
+		testFileName, // would prefer to send straight to /dev/stdout, then read cmd.Output(), but that causes weird errors on my laptop
+	);
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stderr
+	err := cmd.Run()
+	assertNoError(test, err)
+	contents, err := os.ReadFile(testFileName)
+	assertNoError(test, err)
+	err = os.Remove(testFileName)
+	assertNoError(test, err)
+
+	assertEqual(test, "Unexpected .env contents", "TEST_VAR=true\n", string(contents))
+}
