f70111e652668dda4a37415db58cb4f9521c010e
Luke Blaney <git@lukeblaney.co.uk>
2024-02-09 21:15:55 +0000

    add concept of linkedCredential in database/storage layer

--- src/storage.go
+++ src/storage.go
@@ -42,6 +42,20 @@ func initDatastore(datastorePath string, dataKeyPath string, loganne LoganneInte
 		`
 		datastore.db.MustExec(sqlStmt)
 	}
+	if !datastore.TableExists("linked_credential") {
+		slog.Info("Creating table `linked_credential`")
+		sqlStmt := `
+		CREATE TABLE "linked_credential" (
+			"clientsystem" TEXT NOT NULL,
+			"clientenvironment" TEXT NOT NULL,
+			"serversystem" TEXT NOT NULL,
+			"serverenvironment" TEXT NOT NULL,
+			"encryptedvalue" BLOB,
+			CONSTRAINT credential_unique UNIQUE (clientsystem, clientenvironment, serversystem)
+		);
+		`
+		datastore.db.MustExec(sqlStmt)
+	}
 
 	return datastore
 }
@@ -60,6 +74,15 @@ type Credential struct {
 	Key            string `json:"key"`
 	EncryptedValue []byte `json:"value_encrypted,omitempty"`
 	PlainValue     string `json:"value_plain,omitempty"`
+}
+
+type LinkedCredential struct {
+	ClientSystem      string `json:"client_system"`
+	ClientEnvironment string `json:"client_environment"`
+	ServerSystem      string `json:"server_system"`
+	ServerEnvironment string `json:"server_environment"`
+	EncryptedValue    []byte `json:"value_encrypted,omitempty"`
+	PlainValue        string `json:"value_plain,omitempty"`
 }
 
 func (credential *Credential) encrypt(dataBlockCipher cipher.AEAD) (err error) {
@@ -81,7 +104,36 @@ func (credential *Credential) decrypt(dataBlockCipher cipher.AEAD) (err error) {
 	return
 }
 
-func (datastore Datastore) getAllCredentialsBySystemEnvironment(system string, environment string) (plainCredentials map[string]string, err error) {
+func (credential *LinkedCredential) decrypt(dataBlockCipher cipher.AEAD) (err error) {
+	nonce := credential.EncryptedValue[:dataBlockCipher.NonceSize()]
+	cipherText := credential.EncryptedValue[dataBlockCipher.NonceSize():]
+	plainBytes, err := dataBlockCipher.Open(nil, nonce, cipherText, nil)
+	credential.PlainValue = string(plainBytes)
+	return
+}
+
+func (datastore Datastore) getAllCredentialsBySystemEnvironment(system string, environment string) (allCredentials map[string]string, err error) {
+	allCredentials, err = datastore.getSimpleCredentialsBySystemEnvironment(system, environment)
+	if err != nil {
+		slog.Warn("Failed to get Simple Credentials", "system", system, "environment", environment, slog.Any("error", err))
+	}
+	clientCredentials, err := datastore.getClientCredentialsBySystemEnvironment(system, environment)
+	if err != nil {
+		slog.Warn("Failed to get Client Credentials", "system", system, "environment", environment, slog.Any("error", err))
+	}
+	for key, value := range clientCredentials {
+		allCredentials[key] = value
+	}
+	serverCredentialCombinedValue, err := datastore.getServerCredentialsBySystemEnvironment(system, environment)
+	if err != nil {
+		slog.Warn("Failed to get Server Credentials", "system", system, "environment", environment, slog.Any("error", err))
+	}
+	if serverCredentialCombinedValue != "" {
+		allCredentials["CLIENT_KEYS"] = serverCredentialCombinedValue
+	}
+	return
+}
+func (datastore Datastore) getSimpleCredentialsBySystemEnvironment(system string, environment string) (plainCredentials map[string]string, err error) {
 	plainCredentials = make(map[string]string)
 	credentialList := []Credential{}
 	err = datastore.db.Select(&credentialList, "SELECT * FROM credential WHERE system = $1 AND environment = $2 ORDER BY key", system, environment)
@@ -95,6 +147,44 @@ func (datastore Datastore) getAllCredentialsBySystemEnvironment(system string, e
 			slog.Warn("Failed to decrypt", "key", credential.Key, slog.Any("error", err))
 		}
 		plainCredentials[credential.Key] = credential.PlainValue
+	}
+	return
+}
+func (datastore Datastore) getClientCredentialsBySystemEnvironment(system string, environment string) (plainCredentials map[string]string, err error) {
+	plainCredentials = make(map[string]string)
+	linkedClientCredentialList := []LinkedCredential{}
+	err = datastore.db.Select(&linkedClientCredentialList, "SELECT * FROM linked_credential WHERE clientsystem = $1 AND clientenvironment = $2 ORDER BY serversystem", system, environment)
+	if err != nil {
+		slog.Warn("Failed to retrieve credentials from datastore", slog.Any("error", err))
+		return
+	}
+	for _, clientCredential := range linkedClientCredentialList {
+		err := clientCredential.decrypt(datastore.dataBlockCipher)
+		if err != nil {
+			slog.Warn("Failed to decrypt", "clientCredential", clientCredential, slog.Any("error", err))
+		}
+		key := strings.ToUpper("key_"+clientCredential.ServerSystem)
+		plainCredentials[key] = clientCredential.PlainValue
+	}
+	return
+}
+func (datastore Datastore) getServerCredentialsBySystemEnvironment(system string, environment string) (plainValue string, err error) {
+	plainValue = ""
+	linkedCredentialList := []LinkedCredential{}
+	err = datastore.db.Select(&linkedCredentialList, "SELECT * FROM linked_credential WHERE serversystem = $1 AND serverenvironment = $2 ORDER BY clientsystem", system, environment)
+	if err != nil {
+		slog.Warn("Failed to retrieve credentials from datastore", slog.Any("error", err))
+		return
+	}
+	for _, credential := range linkedCredentialList {
+		err := credential.decrypt(datastore.dataBlockCipher)
+		if err != nil {
+			slog.Warn("Failed to decrypt", "credential", credential, slog.Any("error", err))
+		}
+		if plainValue != "" {
+			plainValue = plainValue + ";"
+		}
+		plainValue += fmt.Sprintf("%s:%s=%s", credential.ClientSystem, credential.ClientEnvironment, credential.PlainValue)
 	}
 	return
 }
@@ -120,3 +210,41 @@ func (datastore Datastore) updateCredential(system string, environment string, k
 	return
 }
 
+func (datastore Datastore) updateLinkedCredential(client_system string, client_environment string, server_system string, server_environment string) (err error) {
+	credential := LinkedCredential{}
+	credential.ClientSystem = client_system
+	credential.ClientEnvironment = client_environment
+	credential.ServerSystem = server_system
+	credential.ServerEnvironment = server_environment
+	credential.EncryptedValue, err = generateNewEncryptedValue(datastore.dataBlockCipher)
+	if err != nil {
+		return
+	}
+
+	_, err = datastore.db.NamedExec("REPLACE INTO linked_credential(clientsystem, clientenvironment, serversystem, serverenvironment, encryptedvalue) values(:clientsystem, :clientenvironment, :serversystem, :serverenvironment, :encryptedvalue)", credential)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func generateNewEncryptedValue(dataBlockCipher cipher.AEAD)(encryptedValue []byte, err error) {
+	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	length := 32
+	bytes := make([]byte, length)
+	_, err = rand.Read(bytes)
+	if err != nil {
+		return
+	}
+    for ii := 0; ii < length; ii++ {
+        bytes[ii] = characters[int(bytes[ii])%len(characters)]
+    }
+
+	nonce := make([]byte, dataBlockCipher.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return
+	}
+	encryptedValue = dataBlockCipher.Seal(nonce, nonce, bytes, nil)
+	return
+}
--- src/storage_test.go
+++ src/storage_test.go
@@ -35,6 +35,7 @@ func TestUpdatingCredentialNotifiesLoganne(test *testing.T) {
 	assertEqual(test, "Wrong key sent to loganne", "SPECIAL_KEY", lastLoganneCredential.Key)
 	assertEqual(test, "Credential Value sent to loganne", "", lastLoganneCredential.PlainValue)
 }
+
 func TestRetrievingCredentialDoesntNotifyLoganne(test *testing.T) {
 	loganneRequestCount = 0
 	datastorePath := "test_db.sqlite"
@@ -44,4 +45,91 @@ func TestRetrievingCredentialDoesntNotifyLoganne(test *testing.T) {
 	datastore := initDatastore(datastorePath, dataKeyPath, MockLoganne{})
 	datastore.getAllCredentialsBySystemEnvironment("lucos_test", "testing")
 	assertEqual(test, "Call incorrectly made to loganne", 0, loganneRequestCount)
+}
+
+func TestLinkedCredentials(test *testing.T) {
+	datastorePath := "test_db.sqlite"
+	dataKeyPath := "test_data.key"
+	defer os.Remove(datastorePath)
+	defer os.Remove(dataKeyPath)
+	datastore := initDatastore(datastorePath, dataKeyPath, MockLoganne{})
+	datastore.updateLinkedCredential("lucos_test_client", "testing", "lucos_test_server", "testing")
+	clientCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client", "testing")
+	assertNoError(test, err)
+	clientKey := clientCreds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Client not given credential", "", clientKey)
+	serverCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_server", "testing")
+	assertNoError(test, err)
+	expected := "lucos_test_client:testing="+clientKey
+	assertEqual(test, "Upexpected CLIENT_KEYS for server", expected, serverCreds["CLIENT_KEYS"])
+}
+func TestMultipleLinkedCredentials(test *testing.T) {
+	datastorePath := "test_db.sqlite"
+	dataKeyPath := "test_data.key"
+	defer os.Remove(datastorePath)
+	defer os.Remove(dataKeyPath)
+	datastore := initDatastore(datastorePath, dataKeyPath, MockLoganne{})
+	datastore.updateLinkedCredential("lucos_test_client1", "testing", "lucos_test_server", "testing")
+	datastore.updateLinkedCredential("lucos_test_client2", "staging", "lucos_test_server", "testing")
+	datastore.updateLinkedCredential("lucos_test_client3", "development", "lucos_test_server", "testing")
+	client1Creds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client1", "testing")
+	assertNoError(test, err)
+	client1Key := client1Creds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Client not given credential", "", client1Key)
+	client2Creds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client2", "staging")
+	assertNoError(test, err)
+	client2Key := client2Creds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Client not given credential", "", client2Key)
+	client3Creds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client3", "development")
+	assertNoError(test, err)
+	client3Key := client3Creds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Client not given credential", "", client3Key)
+	serverCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_server", "testing")
+	assertNoError(test, err)
+	expected := "lucos_test_client1:testing="+client1Key+";lucos_test_client2:staging="+client2Key+";lucos_test_client3:development="+client3Key
+	assertEqual(test, "Upexpected CLIENT_KEYS for server", expected, serverCreds["CLIENT_KEYS"])
+}
+func TestRotateLinkedCredentials(test *testing.T) {
+	datastorePath := "test_db.sqlite"
+	dataKeyPath := "test_data.key"
+	defer os.Remove(datastorePath)
+	defer os.Remove(dataKeyPath)
+	datastore := initDatastore(datastorePath, dataKeyPath, MockLoganne{})
+	datastore.updateLinkedCredential("lucos_test_client", "testing", "lucos_test_server", "testing")
+	clientCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client", "testing")
+	assertNoError(test, err)
+	firstClientKey := clientCreds["KEY_LUCOS_TEST_SERVER"]
+	datastore.updateLinkedCredential("lucos_test_client", "testing", "lucos_test_server", "testing")
+	clientCreds, err = datastore.getAllCredentialsBySystemEnvironment("lucos_test_client", "testing")
+	assertNoError(test, err)
+	secondClientKey := clientCreds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Same client key after update", firstClientKey, secondClientKey)
+	serverCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_server", "testing")
+	assertNoError(test, err)
+	expected := "lucos_test_client:testing="+secondClientKey
+	assertEqual(test, "Upexpected CLIENT_KEYS for server", expected, serverCreds["CLIENT_KEYS"])
+}
+func TestUpdatingLinkToDifferentEnv(test *testing.T) {
+	datastorePath := "test_db.sqlite"
+	dataKeyPath := "test_data.key"
+	defer os.Remove(datastorePath)
+	defer os.Remove(dataKeyPath)
+	datastore := initDatastore(datastorePath, dataKeyPath, MockLoganne{})
+	datastore.updateLinkedCredential("lucos_test_client", "testing", "lucos_test_server", "testing")
+	clientCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_client", "testing")
+	assertNoError(test, err)
+	firstClientKey := clientCreds["KEY_LUCOS_TEST_SERVER"]
+	datastore.updateLinkedCredential("lucos_test_client", "testing", "lucos_test_server", "staging")
+	clientCreds, err = datastore.getAllCredentialsBySystemEnvironment("lucos_test_client", "testing")
+	assertNoError(test, err)
+	secondClientKey := clientCreds["KEY_LUCOS_TEST_SERVER"]
+	assertNotEqual(test, "Same client key after update", firstClientKey, secondClientKey)
+
+	serverCreds, err := datastore.getAllCredentialsBySystemEnvironment("lucos_test_server", "testing")
+	assertNoError(test, err)
+	assertEqual(test, "Expected no credentials for server in old environment", map[string]string{}, serverCreds)
+	serverCreds, err = datastore.getAllCredentialsBySystemEnvironment("lucos_test_server", "staging")
+	assertNoError(test, err)
+	expected := "lucos_test_client:testing="+secondClientKey
+	assertEqual(test, "Upexpected CLIENT_KEYS for server in new environment", expected, serverCreds["CLIENT_KEYS"])
 }
