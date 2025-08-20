6f4e73a3c967abbf12466659b9520003dbb43fe8
Luke Blaney <git@lukeblaney.co.uk>
2025-08-20 23:32:08 +0100

    add lucos auth to UI

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from 'express';
+import { middleware as authMiddleware } from './auth.js';
+
 const app = express();
+app.auth = authMiddleware;
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => app.auth(req, res, next));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`UI listening on port ${port}`)
-})
+});
