806d4a415d11d703222c92567472567d6b905eba
Luke Blaney <git@lukeblaney.co.uk>
2026-01-07 22:23:56 +0000

    Add button to UI to copy credential values to clipboard Fixes #62

--- ui/src/client/index.js
+++ ui/src/client/index.js
@@ -1,1 +1,16 @@
 import 'lucos_navbar';
+
+document.querySelectorAll(".view-credential .value").forEach(valueNode => {
+	const text = valueNode.textContent;
+	const copyButton = document.createElement("button");
+	copyButton.textContent = "📋";
+	copyButton.classList.add("copy-button");
+	copyButton.title = "Copy this value to clipboard";
+	copyButton.addEventListener("click", async event => {
+		await navigator.clipboard.writeText(valueNode.textContent);
+		copyButton.dataset.copied = true;
+		copyButton.offsetHeight; // Force a repaint for the transition effect to take place
+		delete copyButton.dataset.copied;
+	});
+	valueNode.parentNode.appendChild(copyButton);
+});
